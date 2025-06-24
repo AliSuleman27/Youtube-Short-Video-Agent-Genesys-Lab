9fed76c56a83c5b605b6ccf1b14173c3d79055b4
AliSuleman27 <alisuleman9721@gmail.com>
2025-06-24 13:01:58 +0500

    added code

---
+++ llm_config.py
@@ -0,0 +1,14 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from config import *
+from dotenv import load_dotenv
+
+
+# Loading the environment
+load_dotenv()
+
+# Creat the Chat Interface
+gemini_llm=ChatGoogleGenerativeAI(model="gemini-1.5-flash",
+                           verbose=LLM_VERBOSE,
+                           temperature=LLM_TEMPERATURE,
+                           google_api_key=os.getenv("GOOGLE_API_KEY"))
+
